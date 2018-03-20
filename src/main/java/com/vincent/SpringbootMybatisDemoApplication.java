707a2cb51540acc4f0d60ee7f0eb5a3d7429f901
longtanfei <longtanfeiwj59672@touna.cn>
2018-03-20 19:34:24 +0800

    实现多数据源切换

--- src/main/java/com/vincent/SpringbootMybatisDemoApplication.java
+++ src/main/java/com/vincent/SpringbootMybatisDemoApplication.java
@@ -1,7 +1,9 @@
 package com.vincent;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class SpringbootMybatisDemoApplication {
